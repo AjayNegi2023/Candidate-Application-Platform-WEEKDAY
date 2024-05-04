fa961edead98ce4004fe7a8981fbfa31a9f78f31
Ajay2023 <ajayn1803@gmail.com>
2024-05-04 21:15:59 +0530

    Fetching Data from Weekday API

---
+++ src/Hooks/useWeekdayData.js
@@ -0,0 +1,43 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { addDetails } from "../Redux/jobSlice";
+
+const useWeekdayData = () => {
+    const dispatch = useDispatch();
+
+
+    const getJobDetails = async() => {
+        const URL = "https://api.weekday.technology/adhoc/getSampleJdJSON";
+        try {
+            const response = await fetch(URL, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    limit: 100,
+                    offset: 0
+                })
+            });
+
+            if (!response.ok) {
+                throw new Error("Failed to fetch data");
+            }
+
+            const data = await response.json();
+            console.log("data", data);
+
+            dispatch(addDetails(data.jdList))
+
+
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    useEffect(() => {
+        getJobDetails();
+    }, []);
+};
+
+export default useWeekdayData;
