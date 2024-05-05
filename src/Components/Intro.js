8bac30d1ae0802d77ea433b3ad68eebc5bc162c2
Ajay2023 <ajayn1803@gmail.com>
2024-05-05 15:56:03 +0530

    Intro.js

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import SearchJob from './SearchJob'
+import SearchJob from './FilterSearch/SearchJob'
 import JobCards from './JobCards/JobCards'
 import useWeekdayData from '../Hooks/useWeekdayData'
 import { useSelector } from 'react-redux'
@@ -7,18 +7,18 @@ const Intro = () => {
 
   useWeekdayData();
 
-  const jobs = useSelector((store)=> store.job);
-  console.log("intro",jobs.data)
+  const jobs = useSelector((store) => store.job);
+
   return (
     <div  >
-        <div>
-            <SearchJob/>
-        </div>
-        <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+      <div>
+        <SearchJob />
+      </div>
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
         {
-        jobs && jobs.data.map(job => <JobCards key={job.jdUid} job={job} />)
+          jobs && jobs.data.map(job => <JobCards key={job.jdUid} job={job} />)
         }
-        </div>
+      </div>
     </div>
   )
 }
