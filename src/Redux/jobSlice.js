87248aaf1cb2bd0e3f7d5365c777c7df05eb69f1
Ajay2023 <ajayn1803@gmail.com>
2024-05-05 15:59:52 +0530

    Job Slice

--- src/Redux/jobSlice.js
+++ src/Redux/jobSlice.js
@@ -1,21 +1,38 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { filterJobs } from "../Components/FilterSearch/FilterJobs";
 
 const jobSlice = createSlice({
     name: "job",
     initialState: {
-        data: []
+        data: [],
+        filters: {
+            role: "",
+            experience: "",
+            remote: "",
+            minBasePay: "",
+            companyName: "",
+            location: "",
+            techStack: "",
+        },
+        originalData: [],
+        isLoading: false,
     },
-
     reducers: {
         addDetails: (state, action) => {
-            state.data = action.payload
+            state.data = action.payload;
+            state.originalData = action.payload;
+        },
+        updateFilters: (state, action) => {
+            state.filters = { ...state.filters, ...action.payload };
+            const activeFilters = Object.keys(state.filters).filter(filter => state.filters[filter] !== "");
+            state.data = filterJobs(state.originalData, state.filters, activeFilters);
         },
         setIsLoading: (state, action) => {
             state.isLoading = action.payload;
-          },
+        },
     }
 });
 
-export const { addDetails , setIsLoading } = jobSlice.actions;
+export const { addDetails, updateFilters, setIsLoading } = jobSlice.actions;
 
 export default jobSlice.reducer;
